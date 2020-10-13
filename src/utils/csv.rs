2f60d3d996cf5f1c44317dfa481a68558b366284
Philippe Vinchon <p.vinchon@gmail.com>
2020-10-13 17:14:45 +0100

    Remove CSVs

--- src/utils/csv.rs
+++
@@ -1,9 +0,0 @@
-use serde::de::DeserializeOwned;
-
-pub fn csv<A: DeserializeOwned>(path: &str) -> Vec<A> {
-    csv::ReaderBuilder::new()
-        .trim(csv::Trim::All)
-        .from_path(path)
-        .map(|mut it| it.deserialize::<A>().flatten().collect())
-        .unwrap_or_else(|err| panic!("Failed parsing CSV file: {}, {}", path, err))
-}
