669fe33af544617fb0f24d96c30d879f0c263dcd
Philippe Vinchon <p.vinchon@gmail.com>
2020-10-31 10:10:43 +0000

    Add service record to game

--- src/games/graphql.rs
+++ src/games/graphql.rs
@@ -3,6 +3,7 @@ use juniper::graphql_object;
 use crate::games::game::{Game, GameProperties};
 use crate::graphql::context::Context;
 use crate::missions::mission::Mission;
+use crate::service_records::service_record::ServiceRecord;
 
 #[graphql_object(Context = Context)]
 impl Game {
@@ -18,5 +19,16 @@ impl Game {
         context
             .missions_dao()
             .all_by_game_id(GameProperties::from(self).id())
+    }
+
+    async fn service_record_by_player(
+        &self,
+        player: String,
+        context: &Context,
+    ) -> Option<Vec<ServiceRecord>> {
+        context
+            .service_records_doa()
+            .find_by_player_and_game(player, self)
+            .await
     }
 }
