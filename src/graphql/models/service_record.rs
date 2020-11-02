54e55079ed5c2e9fe98480242936fe5a2b788bd0
Philippe Vinchon <p.vinchon@gmail.com>
2020-11-02 12:38:36 +0000

    Add game and mission to service record

--- src/graphql/models/service_record.rs
+++ src/graphql/models/service_record.rs
@@ -2,8 +2,10 @@ use juniper::graphql_object;
 
 use crate::campaign_modes::campaign_mode::CampaignMode;
 use crate::difficulties::difficulty::Difficulty;
+use crate::games::game::Game;
 use crate::graphql::context::Context;
 use crate::graphql::models::time::Time;
+use crate::missions::mission::Mission;
 use crate::service_records::service_record::{ServiceRecord, ServiceRecordRun};
 
 #[graphql_object(Context = Context)]
@@ -16,8 +18,18 @@ impl ServiceRecord {
         self.game_id()
     }
 
+    fn game(&self, context: &Context) -> Option<Game> {
+        context.games_dao().find_by_id(self.game_id())
+    }
+
     fn mission_id(&self) -> i32 {
         self.mission_id()
+    }
+
+    fn mission(&self, context: &Context) -> Option<Mission> {
+        context
+            .missions_dao()
+            .find_by_game_id_and_id(self.game_id(), self.mission_id())
     }
 
     fn runs(&self) -> Vec<ServiceRecordRun> {
