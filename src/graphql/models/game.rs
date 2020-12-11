e3784f688cefc23c675b8732485c4d443f98b0f9
Philippe Vinchon <p.vinchon@gmail.com>
2020-12-11 21:38:36 +0000

    Scalar GameId and MissionId

--- src/graphql/models/game.rs
+++ src/graphql/models/game.rs
@@ -1,14 +1,33 @@
-use juniper::graphql_object;
+use juniper::{graphql_object, ParseScalarResult, Value};
 
-use crate::games::game::Game;
+use crate::games::game::{Game, GameId};
 use crate::graphql::context::Context;
 use crate::missions::mission::Mission;
 use crate::service_records::service_record::ServiceRecord;
 
+#[juniper::graphql_scalar(description = "Game id")]
+impl<S> GraphQLScalar for GameId
+where
+    S: ScalarValue,
+{
+    // Define how to convert your custom scalar into a primitive type.
+    fn resolve(&self) -> Value {
+        Value::scalar(*self.value())
+    }
+
+    fn from_input_value(_v: &InputValue) -> Option<Self> {
+        unimplemented!();
+    }
+
+    fn from_str<'a>(_value: ScalarToken<'a>) -> ParseScalarResult<'a, S> {
+        unimplemented!();
+    }
+}
+
 #[graphql_object(Context = Context)]
 impl Game {
-    fn id(&self) -> i32 {
-        *self.id().value()
+    fn id(&self) -> GameId {
+        self.id()
     }
 
     fn name(&self) -> String {
